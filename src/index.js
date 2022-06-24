58a671c02dc9ed5d07a2bd1ce97decc6dbafd56c
Rishi Sanghvi <rishisanghvi2@gmail.com>
2022-06-25 00:47:19 +0530

    reduxPersist

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ import { Provider } from "react-redux";
 
 import App from "./App";
 
-import { store } from "./store/store";
+import { store, persistor } from "./store/store";
+
+import { PersistGate } from "redux-persist/integration/react";
 
 import "./index.scss";
 //import { CategoriesProvider } from "./context/categories.context";
@@ -15,15 +17,11 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        {/* <UserProvider> */}
-        {/* <CategoriesProvider> */}
-        {/* <CartProvider> */}
-        <App />
-        {/* </CartProvider> */}
-        {/* </CategoriesProvider> */}
-        {/* </UserProvider> */}
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
